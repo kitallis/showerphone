a005139131fdc50b921b1eed57543a35d95dcda8
Jithu Gopal <jithug87@gmail.com>
2014-05-21 13:41:22 +0530

    kicking it up

--- src/showerphone/core.clj
+++ src/showerphone/core.clj
@@ -1,29 +1,27 @@
 (ns showerphone.core
-  (:require [overtone.live :refer :all]))
-
-(definst saw-wave [freq 440 attack 0.01 sustain 0.4 release 0.1 vol 0.4]
-  (* (env-gen (env-lin attack sustain release) 1 1 0 1 FREE)
-     (saw freq)
-     vol))
-
-(defn note->hz [music-note]
-  (midi->hz (note music-note)))
-
-(defn new-wave [music-note]
-  (saw-wave (note->hz music-note)))
+  (:require [overtone.live :refer :all]
+            [overtone.inst.sampled-piano :refer :all]))
 
 (defn play-chord [chord]
-  (doseq [note chord] (new-wave note)))
+  (doseq [note chord] (sampled-piano note)))
 
-(defonce metro (metronome 120))
+(defonce one-twenty-bpm (metronome 120))
 
 (defn chord-progression-beat [m beat-num]
   (at (m (+ 0 beat-num)) (play-chord (chord :C4 :major)))
   (at (m (+ 4 beat-num)) (play-chord (chord :G3 :major)))
   (at (m (+ 6 beat-num)) (play-chord (chord :F3 :sus4)))
   (apply-at (m (+ 8 beat-num)) chord-progression-beat m (+ 8 beat-num) [])
-  )
+)
 
-(chord-progression-beat metro (metro))
+(def kick
+  (sample (freesound-path 777)))
 
+(defn looper [nome sound]
+  (let [beat (nome)]
+    (at (nome beat) (sound))
+    (apply-by (nome (inc  beat)) looper nome sound [])))
+
+(looper one-twenty-bpm kick)
+(chord-progression-beat one-twenty-bpm (one-twenty-bpm))
 (stop)
