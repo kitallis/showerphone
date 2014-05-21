c54bc69c3ce68634a3875796b9c9e51beee565c3
Akshay Gupta <kitallis@gmail.com>
2014-05-21 15:14:30 +0530

    updating the lein project settings

--- project.clj
+++ project.clj
@@ -1,8 +1,6 @@
-(defproject insane-noises "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+(defproject showerphone "0.1.0"
+  :description "overtone playground"
+  :url "https://github.com/kitallis/showerphone"
   :dependencies [
                  [org.clojure/clojure "1.5.1"]
                  [overtone "0.9.1"]
